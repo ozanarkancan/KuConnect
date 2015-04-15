211c67c3c5b96accd2cd07560ad28e71159c8d4c
ozanarkancan <can.ozanarkan@gmail.com>
2015-04-15 20:09:22 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='kuconnect',
+    packages=setuptools.find_packages(),
+    author='Ozan Arkan Can',
+    author_email='can.ozanarkan@gmail.com',
+    license='MIT'
+)
